c84061fd789a56cfc89de24fed05a0183236204f
Placido Costa <evoslda2025@gmail.com>
2026-03-28 09:06:37 +0100

    fix: UserCard usa fullName e tipo centralizado

--- components/UserCard.tsx
+++ components/UserCard.tsx
@@ -11,7 +11,7 @@ interface UserCardProps {
 export default function UserCard({ user }: UserCardProps) {
   return (
     <div className="p-4 border rounded mb-2">
-      <h2 className="text-lg font-semibold">{user.name}</h2>
+      <h2 className="text-lg font-semibold">{user.fullName}</h2>
     </div>
   );
 }
