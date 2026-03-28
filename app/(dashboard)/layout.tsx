88ad38215753fe214b21bbdf96e8a44cf1672819
Placido Costa <evoslda2025@gmail.com>
2026-03-28 14:36:10 +0100

    fix: mover client components para DashboardShell

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -7,3 +7,4 @@ export default function DashboardLayout({
 }) {
   return <DashboardShell>{children}</DashboardShell>;
 }
+ 
--- components/DashboardShell.tsx
+++ components/DashboardShell.tsx
@@ -36,4 +36,3 @@ export default function DashboardShell({
     </div>
   );
 }
- 
