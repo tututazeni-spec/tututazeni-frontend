23824f2fc3c806816d3f252fcd0067e102433f14
tututazeni-spec <tututazeni@gmail.com>
2026-08-22 16:40:27 +0100

    chore(test): add component-testing infra (jsdom, Testing Library, tsconfig-paths)

---
+++ vitest.setup.ts
@@ -0,0 +1,4 @@
+// vitest.setup.ts
+// Regista os matchers do @testing-library/jest-dom (toBeInTheDocument,
+// toBeDisabled, etc.) globalmente para todos os testes de componentes.
+import '@testing-library/jest-dom/vitest';
